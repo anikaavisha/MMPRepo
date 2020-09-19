00a489ea836e71a83fad3857d8385e7ce7f661a3
anikaavisha <ektajain11@hotmail.com>
2020-09-18 22:25:29 -0400

    Adding

---
+++ MMP/src/main/java/org/iit/util/BaseClass.java
@@ -0,0 +1,34 @@
+package org.iit.util;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseClass {
+	
+		protected WebDriver driver;
+		
+		
+		@BeforeTest
+		public void openBrowser() {
+			WebDriverManager.chromedriver().setup();
+			// System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+			driver = new ChromeDriver();
+
+		}
+//		@AfterTest
+//		public void closeBrowser(){
+//			driver.close();
+//		}
+//		
+		
+		
+		
+
+}
